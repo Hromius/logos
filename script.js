3d3c19bb947ee0aa8fbeb6004b2744242bef9e42
Hromius <91595187+Hromius@users.noreply.github.com>
2021-10-15 20:36:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+new Swiper  ('.swiper', {
+pagination:{
+el:'.swiper-pagination',
+clickable:true
+}
+});
+
+var button = document.querySelector('.width-more768__button');
+button.addEventListener('click', function () {
+    if (document.querySelector(".width-more768__button").classList.contains("close")){
+        button.classList.remove('close')
+        button.classList.add('open');
+        document.getElementById("text").innerHTML = "Скрыть";
+    } else {
+        button.classList.remove('open')
+        button.classList.add('close');
+        document.getElementById("text").innerHTML = "Показать всё";
+    }
+  });
